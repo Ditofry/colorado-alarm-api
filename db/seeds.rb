03049dd7670dcceb9f0d01a7ed65d7063a467a9f
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-03-07 13:49:13 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Forecast.create([
+  {zip: 80033, time: Date.new(2016,3,6).to_s},
+  {zip: 80203, time: Date.new(2016,3,6).to_s},
+  {zip: 80206, time: Date.new(2016,3,6).to_s},
+  {zip: 80026, time: Date.new(2016,3,6).to_s},
+  {zip: 80202, time: Date.new(2016,3,6).to_s},
+  {zip: 80211, time: Date.new(2016,3,6).to_s},
+  {zip: 80210, time: Date.new(2016,3,6).to_s},
+  {zip: 80003, time: Date.new(2016,3,6).to_s},
+  {zip: 80204, time: Date.new(2016,3,6).to_s},
+  {zip: 90210, time: Date.new(2016,3,6).to_s}
+])
