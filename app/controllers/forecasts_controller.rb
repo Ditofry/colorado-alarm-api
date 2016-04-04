621f4f54daf963048611056793ef742b7e70a16e
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-04-04 11:16:13 -0600

    removed comments

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -5,11 +5,6 @@ class ForecastsController < ApplicationController
   end
 
   def show
-    # This is a good example of where Rails' magic conventions can possibly
-    # lead to confusion.  It's naming the URL argument "id", when it would
-    # be more intuitive to name it "zip".  We should probably declare the show
-    # route "manually" instead of letting our "resource" declaration do all of
-    # the work.
     @forecast = Forecast.where(zip: params[:zipcode])
     render :json => @forecast
   end
