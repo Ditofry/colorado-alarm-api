ff4f0ac6a21dbcee5bbaa33145ec4defff3ad80d
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-04-06 14:45:04 -0600

    added endpoint tests

--- test/controllers/forecasts_controller_test.rb
+++ test/controllers/forecasts_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ForecastsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  describe "GET 'index'"
+    test "should get index" do
+      get :index
+      assert_response :success
+    end
+
+    test "index should return JSON" do
+      get :index
+      assert_not_nil assigns(:forecasts)
+    end
+  end
+
+  describe "GET 'show'" do
+    before(:each) do
+      @company = Factory(:forecast)
+      get :show, :zipcode => @company.id
+    end
+
+    it "should be successful" do
+       response.should be_success
+    end
+
+    it "should return the correct company when correct id is passed" do
+      body = JSON.parse(response.body)
+      body["id"].should == @company.id
+    end
+  end
+
 end
