845aaaaeacc3e89ba7c74cb671af64dc9b2fac1b
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-04-15 15:19:31 -0600

    figaro for key management

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,13 +1,16 @@
 require 'forecast_io'
 require 'openssl' #evil ssl removing hack for windows since windows doesn't have a root store. I am not going to lie, this is sleezy.
+
 #Hack: http://stackoverflow.com/questions/170956/how-can-i-find-which-operating-system-my-ruby-program-is-running-on Just testing.
 def isWindows?
   (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
 end
+
 if isWindows? then
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   #that's evil code. It just disables cert verification on my machine since I am windows and windows doesn't work with ruby on ssl (in the amount of time I have to debug this).
 end
+
 class Forecast < ActiveRecord::Base
 validates :zip, presence:true, length: { in: 5..5 }
 validates_inclusion_of :lat, allow_nil: true, :in => -90..90
@@ -18,8 +21,7 @@ attr_accessor :forecast #remove or privatize this later if it's evil, I want it 
     if (self.lat.nil? or self.lng.nil?)
       return "error!" #should actually throw an error.
     end
-    keyFile = open(Rails.root.join("app", ".api_key"), "r")
-    apiKey = keyFile.readline
+    apiKey = Figaro.env.forecast_io_key # could also use ENV['forecast_io_key']
     keyFile.close
     ForecastIO.api_key = apiKey
     self.forecast = ForecastIO.forecast(self.lat, self.lng, params: { units: 'us' })
