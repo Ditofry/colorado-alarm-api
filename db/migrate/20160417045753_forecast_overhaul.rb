53efb780890331eb7e564883efd1b90f4a3e061e
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-04-17 20:43:49 -0600

    small change in time format

--- db/migrate/20160417045753_forecast_overhaul.rb
+++ db/migrate/20160417045753_forecast_overhaul.rb
@@ -1,10 +1,10 @@
 class ForecastOverhaul < ActiveRecord::Migration
   def change
     change_table :forecasts do |t|
-      t.remove :zip, :lat, :lng, :temp, :precip
+      t.remove :zip, :lat, :lng, :temp, :precip, :time
       t.string :summary, :icon, :precip_type
       t.decimal :precip_intensity, :precip_probability, :temperature, :apparent_temperature, :dew_point, humidity: 0.94, wind_speed: 2.33, wind_bearing: 238, cloud_cover: 0.73, pressure: 1023.48, ozone: 341.9
-      t.integer :visibility
+      t.integer :visibility, :time
       t.references :city
     end
   end
