faf5674c63f0922501199e2d7be92a95ee0defe3
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-04-24 20:54:16 -0600

    flipped lat lon

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,7 +3,7 @@ class ForecastsController < ApplicationController
   def index
     lat = params[:lat]
     lon = params[:lon]
-    city = Geocoder.search("#{lat},#{lon}").first.city
+    city = Geocoder.search("#{lon},#{lat}").first.city
     @forecast = City.where(name: city).first.forecasts.first
     render :json => @forecast
   end
